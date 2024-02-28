8085c829547dd80e4237b1ec856c7e84ffed32fa
MadOne <madone85@googlemail.com>
2024-02-28 21:40:20 +0100

    add servermanager, cleanup

--- CMakeFiles/usbip_gui.dir/parseremote.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/usbip_gui.dir/parseremote.cpp.o: \
- /home/madone/Dokumente/coding/usbip_gui/usbip_gui/parseremote.cpp \
- /usr/include/stdc-predef.h \
- /home/madone/Dokumente/coding/usbip_gui/usbip_gui/parseremote.h
